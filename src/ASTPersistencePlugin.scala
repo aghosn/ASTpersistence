a41147182bf9e020d8c15640d9ca71b56acad393
aghosn <ghosn.adrien@gmail.com>
2014-02-24 14:52:42 +0100

    Imported the project in eclipse and added the library for scala compiler

--- src/ASTPersistencePlugin.scala
+++ src/ASTPersistencePlugin.scala
@@ -4,12 +4,17 @@ import scala.tools.nsc.plugins.{ Plugin, PluginComponent }
 
 /**
  * @brief main class of the plugin.
- */
+ */ 
 class ASTPersistencePlugin(global: Global) extends Plugin {
 
-  val name = ???
-  val description = ???
-  val components = ???
+  val name = "AST persistence plugin"
+  val description = """Saves the AST into the .class. 
+	For more information https://github.com/aghosn/ASTpersistence""""
+  val components = List[PluginComponent](Component) // Might change name
+  
 
   /* TODO : the plugin component, the new phase, and the traverser */
+  private object Component extends PluginComponent{
+	
+  }
 }
