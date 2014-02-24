99e131130c8dd5e1782a859a70b4d8b0d122c970
Mathieu Demarne <anarazel@Fecor>
2014-02-24 13:05:35 +0100

    Structure of the project with empty classes. build.scala for SBT to be fulfill also.

---
+++ project/build.scala
@@ -0,0 +1,13 @@
+import sbt._
+import Keys._
+
+/**
+ * @brief Build properties for SBT.
+ * 
+ */
+class build extends Build {
+	/* TODO : set the properties for :
+	 * 1/ Compilation.
+	 * 2/ Run (directly plugged-in to the compiler).
+	 * 3/ Tests : run the test from the test/ folder. */
+}
---
+++ src/ASTPersistencePlugin.scala
@@ -0,0 +1,15 @@
+
+import scala.tools.nsc.{ Global, Phase }
+import scala.tools.nsc.plugins.{ Plugin, PluginComponent }
+
+/**
+ * @brief main class of the plugin.
+ */
+class ASTPersistencePlugin(global: Global) extends Plugin {
+
+  val name = ???
+  val description = ???
+  val components = ???
+
+  /* TODO : the plugin component, the new phase, and the traverser */
+}
