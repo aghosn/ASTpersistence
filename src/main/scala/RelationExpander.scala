7616c94cef5220c1092e7df9f8f3a57bee365b1a
Mathieu Demarne <anarazel@Fecor>
2014-03-09 16:28:37 +0100

    Addition of some doc for the RelationExpander

--- src/main/scala/RelationExpander.scala
+++ src/main/scala/RelationExpander.scala
@@ -1,6 +1,12 @@
 /**
- * @brief From a given context, retrives the list of the names, of the 
+ * @brief From a given context, retrieves the list of the names, of the
  * symbols, and of the types.
+ *
+ * Passed to the expander : the ASTs
+ * Output of the expander :
+ * 	1. The list of names in BFS order
+ *  2. The list of symbols in BFS order
+ *  3. The list of types in BFS order
  */
 class RelationExpander {
 
