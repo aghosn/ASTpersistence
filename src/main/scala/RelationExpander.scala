b992977fddfc9db99ba00a3ce475224522184ee2
aghosn <ghosn.adrien@gmail.com>
2014-03-09 16:26:56 +0100

    Just going through tree in BFS

--- src/main/scala/RelationExpander.scala
+++ src/main/scala/RelationExpander.scala
@@ -1,7 +1,25 @@
 /**
- * @brief From a given context, retrives the list of the names, of the 
+ * @brief From a given context, retrieves the list of the names, of the 
  * symbols, and of the types.
  */
-class RelationExpander {
+import scala.tools.nsc.{ Global, Phase }
+import scala.collection.mutable.Queue
+import scala.reflect.api.Universe
+
+
+class RelationExpander(val global : Global){
+  import global._
+  
+  def generateBFS(tree : Tree) = {
+	def recBFS(tree : Tree , queue : Queue[Tree]) = queue.get(0) match {
+	  case Some(node) => {
+	    node.children.foreach(n => queue.enqueue(n))
+	   queue.dequeue()
+	  }
+	  case None => println("End of queue");
+	}
+	recBFS(tree, new Queue[Tree]())
+  } 
+    
 
 }
